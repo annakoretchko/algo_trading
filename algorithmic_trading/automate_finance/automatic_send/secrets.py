9ca1b9307c4d477888e5f2e7f6d4f57a03ca3399
annakoretchko <37348349+annakoretchko@users.noreply.github.com>
2021-10-06 17:22:57 -0400

    Delete secrets.py

--- algorithmic_trading/automate_finance/automatic_send/secrets.py
+++
@@ -1,8 +0,0 @@
-MAIL_USERNAME = 'annabee97@gmail.com'
-MAIL_PASSWORD = 'ab1949ACK'
-CELL_NUMBER  = '8636023282'
-
-CONSUMER_KEY = 'ACWGSYFS4FRI51UUEGMRUHU7AHTJYWEG'
-REDIRECT_URL = 'http://localhost/td'
-ACCOUNT_ID = "492476213"
-CREDENTIALS_FILE = "/Users/anna/Dev/automate_finance/td-ameritrade-python-api-master/credentials.json"
